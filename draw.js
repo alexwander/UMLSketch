124821a6fd4b503fdc22b804d531f49ae47badf8
alexwander <ryndyuk.alexander@gmail.com>
2016-02-27 12:46:07 +0200

    rearrange style

--- draw.js
+++ draw.js
@@ -3,55 +3,55 @@
  */
 
 
-
-var canvas;
-var context;
 window.onload = function() {
-// Get the canvas and the drawing context.
-    canvas = document.getElementById("drawingCanvas");
-    context = canvas.getContext("2d");
-    // Attach the events that you need for drawing.
-    canvas.onmousedown = startDrawing;
-    canvas.onmouseup = stopDrawing;
-    canvas.onmouseout = stopDrawing;
-    canvas.onmousemove = draw;
-
 
     var canvas = document.getElementById("drawingCanvas");
     var context = canvas.getContext("2d");
-// Set the line width and color (for all the lines).
-    context.lineWidth = 20;
-    context.strokeStyle = "rgb(205,40,40)";
-// Draw the first line, with the standard butt ending.
-    context.moveTo(10,50);
-    context.lineTo(400,50);
-    context.lineCap = "butt";
-    context.stroke();
-// Draw the second line, with a round cap.
-    context.beginPath();
-    context.moveTo(10,120);
-    context.lineTo(400,120);
-    context.lineCap = "round";
-    context.stroke();
-// Draw the third line, with a square cap.
-    context.beginPath();
-    context.moveTo(10,190);
-    context.lineTo(400,190);
-    context.lineCap = "square";
+    context.moveTo(250, 50);
+    context.lineTo(50, 250);
+    context.lineTo(450, 250);
+    context.closePath();
+// Paint the inside.
+    context.fillStyle = "blue";
+    context.fill();
+// Draw the outline.
+    context.lineWidth = 10;
+    context.strokeStyle = "red";
     context.stroke();
 
 
-
-};
-
-// Keep track of the previous clicked <img> element for color.
-var previousColorElement;
-function changeColor(color, imgElement) {
-// Change the current drawing color.
-    context.strokeStyle = color;
-// Give the newly clicked <img> element a new style.
-    imgElement.className = "Selected";
-// Return the previously clicked <img> element to its normal state.
-    if (previousColorElement != null) previousColorElement.className = "";
-    previousColorElement= imgElement;
 }
+
+
+
+
+//var canvas;
+//var context;
+//window.onload = function() {
+//// Get the canvas and the drawing context.
+//    canvas = document.getElementById("drawingCanvas");
+//    context = canvas.getContext("2d");
+//    // Attach the events that you need for drawing.
+//    canvas.onmousedown = startDrawing;
+//    canvas.onmouseup = stopDrawing;
+//    canvas.onmouseout = stopDrawing;
+//    canvas.onmousemove = draw;
+//
+//
+//
+//
+//
+//
+//};
+//
+//// Keep track of the previous clicked <img> element for color.
+//var previousColorElement;
+//function changeColor(color, imgElement) {
+//// Change the current drawing color.
+//    context.strokeStyle = color;
+//// Give the newly clicked <img> element a new style.
+//    imgElement.className = "Selected";
+//// Return the previously clicked <img> element to its normal state.
+//    if (previousColorElement != null) previousColorElement.className = "";
+//    previousColorElement= imgElement;
+//}
